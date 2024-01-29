86b06d425d3b6e8a669e9a22aa225bfd83605121
Xereah <patrykstruzik@onet.pl>
2024-01-29 18:46:10 +0100

    fist comit

---
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -0,0 +1,13 @@
+<aside class="bg-gray-200 w-1/4 h-full fixed left-0 top-0 overflow-y-auto">
+    <!-- Sidebar Content -->
+    <div class="p-4">
+        <!-- Your sidebar content goes here -->
+        <h2 class="text-lg font-semibold">Sidebar Content</h2>
+        <!-- Example Links -->
+        <ul>
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <!-- Add more sidebar links as needed -->
+        </ul>
+    </div>
+</aside>
